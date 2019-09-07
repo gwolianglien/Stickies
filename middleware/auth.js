578e63105386f46011858f8d35b81c99108279b9
Leo Lien <gwolianglien@gmail.com>
2019-09-07 15:17:15 -0400

    middleware auth

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = (req, res, next) => {
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res.status(401).json({ msg: 'No JWT, Authorization Denied.' });
+    }
+    try {
+        const decodedjwt = jwt.verify(
+            token, 
+            config.get('serv.jwtCode')
+        );
+        req.user = decodedjwt.user;
+        next();
+    } catch(err) {
+        console.error(err.message);
+        res.status(401).json({ msg: 'Token is not valid. '});
+    }
+}
